5bae40aff32e3f74d71eab6afe66351b28fbe3fc
Madhav Nambi <nambimadhavkarthik@gmail.com>
2026-06-29 06:58:32 +0530

    Document ingestion Script

---
+++ src/vectorize_script.py
@@ -0,0 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+from vectorize_book import vectorize_book_and_store_to_db, vectorize_chapters
+
+load_dotenv()
+
+CLASS_SUBJECT_NAME = os.getenv("CLASS_SUBJECT_NAME")
+print(CLASS_SUBJECT_NAME)
+vectorize_book_and_store_to_db(
+    CLASS_SUBJECT_NAME,
+    vector_db_name="class_12_biology_vector_db"
+)
+
+vectorize_chapters(CLASS_SUBJECT_NAME)
